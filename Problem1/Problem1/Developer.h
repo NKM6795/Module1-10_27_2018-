774d506f2c2461eb5d983e7bdaba55f3a96f7ec1
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2018-10-28 18:25:29 +0200

    Add developer class

---
+++ Problem1/Problem1/Developer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "AnnouncementAllClass.h"
+#include "Technology.h"
+
+
+class Developer
+{
+	string name;
+
+	Project *project;
+
+	vector<Technology *> technology;
+
+	float efficiency;
+
+public:
+	Developer() :
+		name(""), project(nullptr), efficiency(0.1f + float(rand() % 90) / 100) {}
+	Developer(string name) :
+		name(name), project(nullptr), efficiency(0.1f + float(rand() % 90) / 100) {}
+	Developer(string name, float efficiency) :
+		name(name), project(nullptr), efficiency(efficiency) {}
+
+};
